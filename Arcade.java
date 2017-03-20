a8a6fe235105fb3cea4178f0c0662538655afc7d
Bento Cruz <bentocru@lancs.local>
2017-03-20 20:30:27 +0000

    First Building game project with comments

--- Arcade.java
+++ Arcade.java
@@ -1,21 +1,22 @@
  import javax.swing.*;
  
- public class Arcade 
+ public class Arcade                           //making a class called Arcade 
  {
- public static void main(String [] arguments) 
+ public static void main(String [] arguments)  //main operator with no return value
  {
- int SCREEN_HEIGHT = 700;
- int SCREEN_WIDTH = 700; 
+ int SCREEN_HEIGHT = 640;                      //set the height of the screen
+ int SCREEN_WIDTH = 800;                       //set the width of the screen
  
- JFrame window = new JFrame(); 
- Racer r = new Racer();
- window.setTitle("Arcade");
- window.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
-  window.setContentPane(r.getPanel());
-  window.setVisible(true);
+  JFrame window = new JFrame();                //make a new frame
+  Racer r = new Racer();                        
+  window.setTitle("Arcade");                   //set the title of the game
+  window.setSize(SCREEN_WIDTH, SCREEN_HEIGHT); //sets window size
+  window.setContentPane(r.getPanel());         //sets game as panel 
   
-   r.start();
-while(r.isPlaying())
-      r.update();
+  window.setVisible(true);                     //makes panel visible
+  
+   r.start();                                  //makes game start
+while(r.isPlaying())                           //making a while statement to start the game again
+      r.update();                              
  }
  }
